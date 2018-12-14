34ddacdf8e36e5eb5706c574010945086cea2dd0
evan.meeks@gmail.com <Gassw0rds9>
2018-12-14 15:09:29 -0600

    add redirect for portfolio

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, IndexRoute, Route } from 'react-router';
+import { Router, Redirect, IndexRoute, Route } from 'react-router';
 
 import App from './App';
 import BlogHome from './BlogHome';
@@ -10,8 +10,8 @@ const Routes = (props) => (
 		<Route path='/' component={App}>
 			<IndexRoute component={BlogHome} />
 			<Route path='/p/:page' component={BlogHome} />
+			<Route path='/post/evan-meeks-my-portfolio' component={BlogHome} />
 			<Route path='/post/:slug' component={BlogPost} />
-			{/* <Route path='/post/streaming-playlist-app' component={BlogPostMd} /> */}
 		</Route>
 	</Router>
 );
